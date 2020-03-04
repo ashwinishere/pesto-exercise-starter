8ca3b234e71a5eb517936a13ea04bc6581ad5362
Aswin <aswin.harikumar@yahoo.com>
2020-03-04 13:48:19 +0530

    sumPrime solved

--- js-exercises/sum-primes/sumPrimes.js
+++ js-exercises/sum-primes/sumPrimes.js
@@ -1,7 +1,21 @@
-function sumPrimes() {
-
+function getPrimeNumbers(max) {
+  const markedNumbers = [];
+  const primeNumbers = [];
+  let i = 2;
+  while (i <= max) {
+    if (!markedNumbers[i]) {
+      primeNumbers.push(i);
+      for (let j = i * 2; j <= max; j += i) {
+        markedNumbers[j] = true;
+      }
+    }
+    i += 1;
+  }
+  return primeNumbers;
 }
-
+function sumPrimes(maximumNumber) {
+  return getPrimeNumbers(maximumNumber).reduce((a, b) => a + b, 0);
+}
 export {
   sumPrimes,
 };
