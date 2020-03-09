ecb62616225ff947c27964b28bf5f09067001c98
Aswin <aswin.harikumar@yahoo.com>
2020-03-09 15:14:44 +0530

    Solve MathFunction

---
+++ js-exercises/math-fns/mathFns.js
@@ -0,0 +1,11 @@
+
+// Don't change the export values.
+// You can use the function in the Math module
+const sqrt = (input) => Math.sqrt(input);
+const power = (baseValue, exponent) => baseValue ** exponent;
+const round = (input) => Math.round(input);
+export {
+  sqrt,
+  power,
+  round,
+};
