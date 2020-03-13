2d6bd45efc09ff5788c57cf90e71bc2b3a38c902
Aswin <aswin.harikumar@yahoo.com>
2020-03-13 17:12:51 +0530

    solve leastCommonMultiple

---
+++ js-exercises/least-common-multiple/leastCommonMultiple.test.js
@@ -0,0 +1,24 @@
+import {
+  leastCommonMultiple,
+} from './leastCommonMultiple';
+
+describe('leastCommonMultiple', () => {
+  test('is giving correct output ', () => {
+    expect(leastCommonMultiple(6, 21)).toStrictEqual(42);
+  });
+  test('is giving correct output ', () => {
+    expect(leastCommonMultiple(12, 124)).toStrictEqual(372);
+  });
+  test('is giving correct output for same numbers', () => {
+    expect(leastCommonMultiple(6, 6)).toStrictEqual(6);
+  });
+  test('is throwing type error ', () => {
+    expect(leastCommonMultiple(6, 'hi')).toThrow(TypeError);
+    expect(leastCommonMultiple(false, 12)).toThrow(TypeError);
+  });
+  test('is throwing negative error ', () => {
+    expect(leastCommonMultiple(-6, 21)).toThrow(Error);
+    expect(leastCommonMultiple(6, -21)).toThrow(Error);
+    expect(leastCommonMultiple(-6, -21)).toThrow(Error);
+  });
+});
