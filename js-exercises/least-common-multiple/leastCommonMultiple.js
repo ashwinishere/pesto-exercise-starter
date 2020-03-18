f1a680fbbeebefa0ccf041c242983e54478e6d40
Aswin <aswin.harikumar@yahoo.com>
2020-03-18 18:35:13 +0530

    fix: throw error on type first
    type error to be thrown first instead of equality check

--- js-exercises/least-common-multiple/leastCommonMultiple.js
+++ js-exercises/least-common-multiple/leastCommonMultiple.js
@@ -1,9 +1,8 @@
 // first one big, second one small , same
 function leastCommonMultiple(num1, num2) {
-  if (num1 === num2) return num1;
-  if (num1 <= 0 || num2 <= 0) throw new Error('Input must be positive');
   if (typeof num1 !== 'number' || typeof num2 !== 'number') throw new TypeError(`Expected input type number but received ${typeof num1} and ${typeof num2}`);
-
+  if (num1 <= 0 || num2 <= 0) throw new Error('Input must be positive');
+  if (num1 === num2) return num1;
   const largeNumber = Math.max(num1, num2);
   const smallerNumber = Math.min(num1, num2);
   let i = largeNumber;
